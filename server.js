41672cfdf7015e6761964cca3a3587b4e45be6bb
AviadC31 <aviadcoh1@gmail.com>
2021-02-05 20:24:22 +0200

    fix

--- server.js
+++ server.js
@@ -169,6 +169,6 @@ app.use(express.static(path.join(__dirname, 'node_modules')))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-const port = process.env.PORT || 80
+const port = process.env.PORT || 4000
 
 server.listen(port, () => console.log(`Server running on port: ${port}`))
